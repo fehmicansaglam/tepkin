f2d7367f272b25ba36ccad21e0685157fd7fcfb7
Fehmi Can Saglam <fehmican.saglam@gmail.com>
2015-02-25 00:56:00 +0200

    Add findAndRemove Java API

--- src/main/scala/net/fehmicansaglam/tepkin/api/MongoCollection.scala
+++ src/main/scala/net/fehmicansaglam/tepkin/api/MongoCollection.scala
@@ -5,7 +5,7 @@ import java.util.{List => JavaList, Optional}
 
 import akka.stream.javadsl.Source
 import akka.util.Timeout
-import net.fehmicansaglam.tepkin.api.FutureConverters.toJava
+import net.fehmicansaglam.tepkin.api.JavaConverters.{toCompletableFuture, toOptional}
 import net.fehmicansaglam.tepkin.bson.BsonDocument
 import net.fehmicansaglam.tepkin.protocol.message.Reply
 import net.fehmicansaglam.tepkin.protocol.result.{CountResult, DeleteResult, InsertResult}
@@ -20,60 +20,65 @@ import scala.concurrent.ExecutionContext
  */
 class MongoCollection(proxy: ScalaCollection) {
 
-  def count(ec: ExecutionContext, timeout: Timeout): CompletableFuture[CountResult] = toJava {
-    proxy.count()(ec, timeout)
-  }(ec)
-
-  def count(query: BsonDocument, ec: ExecutionContext, timeout: Timeout): CompletableFuture[CountResult] = toJava {
-    proxy.count(query = Some(query))(ec, timeout)
-  }(ec)
-
-  def count(query: BsonDocument,
-            limit: Int,
-            ec: ExecutionContext,
-            timeout: Timeout): CompletableFuture[CountResult] = toJava {
-    proxy.count(query = Some(query), limit = Some(limit))(ec, timeout)
-  }(ec)
-
-  def count(query: BsonDocument,
-            limit: Int,
-            skip: Int,
-            ec: ExecutionContext,
-            timeout: Timeout): CompletableFuture[CountResult] = toJava {
-    proxy.count(query = Some(query), limit = Some(limit), skip = Some(skip))(ec, timeout)
-  }(ec)
-
-  def delete(deletes: Array[BsonDocument],
-             ec: ExecutionContext,
-             timeout: Timeout): CompletableFuture[DeleteResult] = toJava {
-    proxy.delete(deletes = deletes)(ec, timeout)
-  }(ec)
-
-  def drop(ec: ExecutionContext, timeout: Timeout): CompletableFuture[Reply] = toJava {
-    proxy.drop()(ec, timeout)
-  }(ec)
-
-  def find(query: BsonDocument,
-           ec: ExecutionContext,
-           timeout: Timeout): CompletableFuture[Source[JavaList[BsonDocument]]] = toJava {
-    import scala.collection.JavaConverters._
-    proxy.find(query)(ec, timeout).map(source => Source.adapt(source.map(_.asJava)))(ec)
-  }(ec)
-
-  def findOne(ec: ExecutionContext, timeout: Timeout): CompletableFuture[Optional[BsonDocument]] = {
-    findOne(BsonDocument.empty, ec, timeout)
+  def count(implicit ec: ExecutionContext, timeout: Timeout): CompletableFuture[CountResult] = toCompletableFuture {
+    proxy.count()
   }
 
-  def findOne(query: BsonDocument,
-              ec: ExecutionContext,
-              timeout: Timeout): CompletableFuture[Optional[BsonDocument]] = toJava {
-    proxy.findOne(query)(ec, timeout).map {
-      case Some(document) => Optional.of[BsonDocument](document)
-      case None => Optional.empty[BsonDocument]()
-    }(ec)
-  }(ec)
+  def count(query: BsonDocument)
+           (implicit ec: ExecutionContext, timeout: Timeout): CompletableFuture[CountResult] = toCompletableFuture {
+    proxy.count(query = Some(query))
+  }
 
-  def insert(document: BsonDocument, ec: ExecutionContext, timeout: Timeout): CompletableFuture[InsertResult] = toJava {
-    proxy.insert(Seq(document))(ec, timeout)
-  }(ec)
+  def count(query: BsonDocument,
+            limit: Int)
+           (implicit ec: ExecutionContext,
+            timeout: Timeout): CompletableFuture[CountResult] = toCompletableFuture {
+    proxy.count(query = Some(query), limit = Some(limit))
+  }
+
+  def count(query: BsonDocument,
+            limit: Int,
+            skip: Int)
+           (implicit ec: ExecutionContext,
+            timeout: Timeout): CompletableFuture[CountResult] = toCompletableFuture {
+    proxy.count(query = Some(query), limit = Some(limit), skip = Some(skip))
+  }
+
+  def delete(deletes: Array[BsonDocument])
+            (implicit ec: ExecutionContext,
+             timeout: Timeout): CompletableFuture[DeleteResult] = toCompletableFuture {
+    proxy.delete(deletes = deletes)
+  }
+
+  def drop(implicit ec: ExecutionContext, timeout: Timeout): CompletableFuture[Reply] = toCompletableFuture {
+    proxy.drop()
+  }
+
+  def find(query: BsonDocument)
+          (implicit ec: ExecutionContext,
+           timeout: Timeout): CompletableFuture[Source[JavaList[BsonDocument]]] = toCompletableFuture {
+    import scala.collection.JavaConverters._
+    proxy.find(query).map(source => Source.adapt(source.map(_.asJava)))
+  }
+
+  def findAndRemove(query: BsonDocument)
+                   (implicit ec: ExecutionContext,
+                    timeout: Timeout): CompletableFuture[Optional[BsonDocument]] = toCompletableFuture {
+    proxy.findAndModify(query = Some(query), removeOrUpdate = Left(true)).map(toOptional)
+  }
+
+  def findOne(implicit ec: ExecutionContext, timeout: Timeout): CompletableFuture[Optional[BsonDocument]] = {
+    findOne(BsonDocument.empty)
+  }
+
+  def findOne(query: BsonDocument)
+             (implicit ec: ExecutionContext,
+              timeout: Timeout): CompletableFuture[Optional[BsonDocument]] = toCompletableFuture {
+    proxy.findOne(query).map(toOptional)
+  }
+
+  def insert(document: BsonDocument)
+            (implicit ec: ExecutionContext, timeout: Timeout): CompletableFuture[InsertResult] = toCompletableFuture {
+    proxy.insert(Seq(document))
+  }
 }
