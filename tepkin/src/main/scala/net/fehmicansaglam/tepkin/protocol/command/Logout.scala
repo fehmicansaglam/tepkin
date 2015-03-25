a438aaa256192dec49dc4cc64b1bb08027eec186
Fehmi Can Saglam <fehmican.saglam@gmail.com>
2015-03-25 22:51:23 +0200

    Add Logout command

---
+++ tepkin/src/main/scala/net/fehmicansaglam/tepkin/protocol/command/Logout.scala
@@ -0,0 +1,12 @@
+package net.fehmicansaglam.tepkin.protocol.command
+
+import net.fehmicansaglam.bson.BsonDocument
+import net.fehmicansaglam.bson.BsonDsl._
+import net.fehmicansaglam.bson.Implicits._
+
+/**
+ * Terminates the current authenticated session
+ */
+case class Logout(databaseName: String) extends Command {
+  override val command: BsonDocument = "logout" := 1
+}
