b792fececaed7624aedba57be6435ee22c3cdf74
Jeroen Rosenberg <jeroen.rosenberg@gmail.com>
2016-02-19 16:52:29 +0100

    Revert "Support deeply nested structures in BSON conversion"
    This reverts commit 62052b04eb040415a69a5ebb0f4cca38161879df.

--- bson/src/test/scala/net/fehmicansaglam/bson/BsonDslSpec.scala
+++ bson/src/test/scala/net/fehmicansaglam/bson/BsonDslSpec.scala
@@ -14,26 +14,5 @@ class BsonDslSpec extends WordSpec with Matchers {
       val actual = $unset("field1", "field2")
       actual should be(expected)
     }
-
-    "handle (deeply) nested structures" in {
-      val map = Map(
-        "level0" -> 1,
-        "level1" -> Map(
-          "level2" -> "2"
-        )
-      )
-      val doc = BsonDocument.from(map.toIterable)
-      doc.toJson() should be("""{ "level0": 1, "level1": { "level2": "2" } }""")
-
-      val map2 = Map(
-        "level1" -> List(Map(
-          "level2" -> "2"
-        )),
-        "listoflists" -> List(List(1,2), List(3,4))
-      )
-      val doc2 = BsonDocument.from(map2.toIterable)
-      doc2.toJson() should be("""{ "level1": [ { "level2": "2" } ], "listoflists": [ [ 1, 2 ], [ 3, 4 ] ] }""")
-
-    }
   }
 }
