5643804c82dbbdedf971d37d4099f8d800ac0a9d
Jeroen Rosenberg <jeroen.rosenberg@gmail.com>
2016-02-24 13:10:03 +0100

    Use embedded mongo for tests

--- tepkin/src/test/scala/com/github/jeroenr/tepkin/GridFsSpec.scala
+++ tepkin/src/test/scala/com/github/jeroenr/tepkin/GridFsSpec.scala
@@ -9,37 +9,50 @@ import com.github.jeroenr.bson.BsonDsl._
 import com.github.jeroenr.bson.Implicits._
 import com.github.jeroenr.bson.util.Converters
 import com.github.jeroenr.bson.{BsonDsl, Implicits}
+import com.github.simplyscala.{MongodProps, MongoEmbedDatabase}
 import org.scalatest._
 import org.scalatest.concurrent.ScalaFutures
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
+@DoNotDiscover
 class GridFsSpec
   extends FlatSpec
   with Matchers
   with ScalaFutures
   with OptionValues
   with BeforeAndAfter
-  with BeforeAndAfterAll {
+  with BeforeAndAfterAll
+  with MongoEmbedDatabase {
 
   override implicit val patienceConfig = PatienceConfig(timeout = 30.seconds, interval = 1.seconds)
 
-  val client = MongoClient("mongodb://localhost")
-  val db = client("tepkin")
-  val fs = db.gridFs()
+  var client: MongoClient = _
+  var db: MongoDatabase = _
+  var collection: MongoCollection = _
+  var fs: GridFs = _
 
-  import client.{context, ec}
+  override protected def beforeAll() = {
+    client = MongoClient("mongodb://localhost:12345")
+    db = client("tepkin")
+    collection = db("mongo_collection_spec")
+    fs = db.gridFs()
+  }
 
   implicit val timeout: Timeout = 30.seconds
 
   before {
+    implicit val context = client.context
+    implicit val ec = client.ec
     Await.ready(db.collection("fs.files").drop(), 5.seconds)
     Await.ready(db.collection("fs.chunks").drop(), 5.seconds)
     Thread.sleep(2000)
   }
 
   after {
+    implicit val context = client.context
+    implicit val ec = client.ec
     Await.ready(db.collection("fs.files").drop(), 5.seconds)
     Await.ready(db.collection("fs.chunks").drop(), 5.seconds)
   }
@@ -47,6 +60,8 @@ class GridFsSpec
   override protected def afterAll() = client.shutdown()
 
   "A GridFs" should "put and find and delete File" in {
+    implicit val context = client.context
+    implicit val ec = client.ec
     implicit val mat = ActorMaterializer()
 
     val result = for {
@@ -63,6 +78,8 @@ class GridFsSpec
   }
 
   it should "put and get and delete File" in {
+    implicit val context = client.context
+    implicit val ec = client.ec
     implicit val mat = ActorMaterializer()
 
     val result = for {
@@ -84,6 +101,8 @@ class GridFsSpec
   }
 
   it should "put and get and delete FileInputStream" in {
+    implicit val context = client.context
+    implicit val ec = client.ec
     implicit val mat = ActorMaterializer()
 
     val result = for {
